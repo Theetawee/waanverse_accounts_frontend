0cc16acd25e4e0068834a0a1cc560cafd0bd8118
Forever Inc dev <redodevs@gmail.com>
2024-02-02 00:03:04 +0300

    Update Seo.tsx

--- src/components/utils/Seo.tsx
+++ src/components/utils/Seo.tsx
@@ -1,65 +1,32 @@
 import { Helmet } from "react-helmet-async";
 
-const Seo = () => {
+const Seo = ({title, description}: {title: string, description: string}) => {
     return (
         <>
             <Helmet>
-                <title>WaanVerse Accounts</title>
+                <title>{title}</title>
                 <meta
                     name="description"
-                    content="Manage and control your authentication flow with WaanVerse Accounts. Streamlined user authentication, secure access, and personalized user profiles."
+                    content={description}
                 />
                 <meta
                     name="keywords"
                     content="WaanVerse, Accounts, Authentication, Security, User Profiles"
                 />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <link rel="canonical" href="https://www.yourwebsite.com/" />
+                <link rel="canonical" href="https://www.waanverse.com/" />
 
                 {/* Open Graph Tags */}
-                <meta property="og:title" content="WaanVerse Accounts" />
+                <meta property="og:title" content={title} />
                 <meta
                     property="og:description"
-                    content="Manage and control your authentication flow with WaanVerse Accounts. Streamlined user authentication, secure access, and personalized user profiles."
+                    content={description}
                 />
-                <meta
-                    property="og:image"
-                    content="URL to an image for sharing"
-                />
-                <meta
-                    property="og:url"
-                    content="https://www.yourwebsite.com/"
-                />
-                <meta property="og:type" content="website" />
-
-                {/* Twitter Card Tags */}
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content="WaanVerse Accounts" />
+<meta name="twitter:title" content={title} />
                 <meta
                     name="twitter:description"
-                    content="Manage and control your authentication flow with WaanVerse Accounts. Streamlined user authentication, secure access, and personalized user profiles."
-                />
-                <meta
-                    name="twitter:image"
-                    content="URL to an image for sharing"
-                />
-                <meta
-                    name="twitter:url"
-                    content="https://www.yourwebsite.com/"
+                    content={description}
                 />
 
-                {/* Robots Meta Tag */}
-                <meta name="robots" content="index, follow" />
-
-                {/* Favicon */}
-                <link
-                    rel="icon"
-                    href="path/to/favicon.ico"
-                    type="image/x-icon"
-                />
             </Helmet>
         </>
     );
