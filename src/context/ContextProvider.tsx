80fd46b2c0ec0a2243704b1262cccd218be173f6
Forever Inc dev <redodevs@gmail.com>
2024-02-02 10:42:00 +0300

    save

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,6 +1,12 @@
 import AuthContextProvider from "./AuthContext";
+import { HelmetProvider } from "react-helmet-async";
+
 const ContextProvider = ({ children }: { children: React.ReactNode }) => {
-    return <AuthContextProvider>{children}</AuthContextProvider>;
+    return (
+        <AuthContextProvider>
+            <HelmetProvider>{children}</HelmetProvider>
+        </AuthContextProvider>
+    );
 };
 
 export default ContextProvider;
