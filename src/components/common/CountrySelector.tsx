b7b574872d6ad0e3d0cda769c3f801ce20b68660
Forever Inc dev <redodevs@gmail.com>
2024-02-02 11:03:44 +0300

    Update CountrySelector.tsx

--- src/components/common/CountrySelector.tsx
+++ src/components/common/CountrySelector.tsx
@@ -7,12 +7,26 @@ function CountrySelector( {sendValue}: {sendValue: (value: any) => void}) {
     const [value, setValue] = useState<any>("");
     const options = useMemo(() => countryList().getData(), []);
 
+    const customStyles = {
+        control: (provided: any) => ({
+            ...provided,
+            padding:"4px",
+            display: "flex",
+            flexWrap: "nowrap",
+        }),
+        menu: (provided:any) => ({
+            ...provided,
+        }),
+    };
+
+
+
     const changeHandler = (value:any) => {
         setValue(value);
         sendValue(value);
     };
 
-    return <Select name="country" options={options} value={value} onChange={changeHandler} />;
+    return <Select styles={customStyles}  name="country" options={options} value={value} onChange={changeHandler} />;
 }
 
 export default CountrySelector;
