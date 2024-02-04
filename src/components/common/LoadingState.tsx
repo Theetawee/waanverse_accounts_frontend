f1c232e342a4adf3ef3303a25c7ceeb2d705a11d
Forever Inc dev <redodevs@gmail.com>
2024-02-04 08:24:41 +0300

    save

--- src/components/common/LoadingState.tsx
+++ src/components/common/LoadingState.tsx
@@ -1,7 +1,11 @@
+import Loader from "./Loader"
 
 const LoadingState = () => {
   return (
-    <div>LoadingState</div>
+    <section className="h-screen flex-col fixed left-0 bottom-0 z-50 w-full flex items-center bg-white justify-center">
+      <Loader />
+      <p>Connecting ...</p>
+    </section>
   )
 }
 
--- src/components/common/OfflineAlert.tsx
+++ src/components/common/OfflineAlert.tsx
@@ -1,8 +1,17 @@
-
+import { MdSignalWifiConnectedNoInternet4 } from "react-icons/md";
 const OfflineAlert = () => {
-  return (
-    <div>OfflineAlert</div>
-  )
-}
+    return (
+        <div
+            className="flex fixed bottom-0 left-1/2 z-[1000] rounded-lg transform -translate-x-1/2 w-[95%] sm:w-[70%] lg:w-[33%]  text-center items-center justify-center  p-4 mb-4   text-gray-300  bg-gray-900 dark:bg-gray-800 "
+            role="alert"
+        >
+            <MdSignalWifiConnectedNoInternet4    className="w-6 h-6" />
+            <span className="sr-only">Info</span>
+            <div className="text-center">
+                <span className="font-medium ml-3">You're currently offline!</span>
+            </div>
+        </div>
+    );
+};
 
-export default OfflineAlert
+export default OfflineAlert;
