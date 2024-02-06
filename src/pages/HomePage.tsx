1b97bac6dd392d789b94b9c68119a464022e2604
Forever Inc dev <redodevs@gmail.com>
2024-02-06 07:28:25 +0300

    save

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -48,7 +48,7 @@ const HomePage = () => {
                             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                                 Elevate Your Online Experience with{" "}
                                 <span className="text-primary-600 underline-offset-8 underline">
-                                    WaanVerse&nbsp;Accounts
+                                    Waanverse&nbsp;Accounts
                                 </span>
                             </h1>
                             <p className="mb-8   font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
