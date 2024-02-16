3b214d81f2a5e25ab430251e69ac3ca903336108
Forever Inc dev <redodevs@gmail.com>
2024-02-17 00:47:36 +0300

    save

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-import { registerSW } from "virtual:pwa-register";
 
-registerSW({ immediate: true });
 
 
 if (process.env.NODE_ENV === "production") {
