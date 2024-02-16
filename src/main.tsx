44472a42819a5e6f37cdcd820cf4b94dcf219a74
Forever Inc dev <redodevs@gmail.com>
2024-02-17 00:20:01 +0300

    save

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+    disableReactDevTools();
+}
+
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
